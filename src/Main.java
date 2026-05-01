9d5f857a7f925abe2809289f336a4f7953f67106
Malakai Unland <mr.unland@ht-la.org>
2026-05-01 11:50:12 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //two - next line
+        //more - print that thing
+        //lines
+    }
+}
